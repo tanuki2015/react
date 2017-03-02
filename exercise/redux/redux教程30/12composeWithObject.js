e1f1ff5c1edf54565190afedd1c4d7e7bbcf74e8
tanuki2015 <75954762@qq.com>
2017-03-03 06:18:50 +0800

    12 compose with object

---
+++ exercise/redux/redux教程30/12composeWithObject.js
@@ -0,0 +1,74 @@
+import {createStore} from 'redux';
+
+const todo = (state,action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false,
+            };
+
+        case 'TOGGLE_TODO':
+            if (action.id !== state.id) {
+                return state;
+            }
+
+            return  {...state, completed: !state.completed};
+
+        default:
+            return state;
+    }
+};
+
+// 改一下名字，addTodo -> todos
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case 'TOGGLE_TODO':
+            return state.map((item) => {
+                return todo(item, action);
+            });
+        default:
+            return state;
+    }
+};
+
+// 添加visibilityFilter
+const visibilityFilter =  (state = 'SHOW_ALL', action) => {
+    switch (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+const todoApp = (state = {}, action) => {
+    return {
+        todos: todos(state.todos, action),
+        visibilityFilter: visibilityFilter(state.filter, action),
+    }
+};
+
+
+const store = createStore(todoApp);
+console.log('initial state');
+console.log(store.getState());
+
+store.dispatch({type: 'ADD_TODO', id: 0, text: 'learn redux', completed: false});
+console.log(store.getState());
+store.dispatch({type: 'ADD_TODO', id: 1, text: 'go to shopping', completed: false});
+console.log(store.getState());
+console.log('----------------');
+
+store.dispatch({type: 'TOGGLE_TODO', id: 1});
+console.log(store.getState());
+
+store.dispatch({type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_COMPLETED'});
+console.log(store.getState());
+
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 hello文件夹是一个基本的模板，已经正确配置了webpack，可以解析es6和react。
 
 新建项目可以用它做基础，拷贝过来用。
+
+问题：
+
+1. 箭头函数自动把this绑定到组件的实例上，需要stage-1的支持，在atom中会报错，但编译可以通过
+```
+tick = () => {
+    this.setState({secondsElapsed: this.state.secondsElapsed + 1});
+}
+```
