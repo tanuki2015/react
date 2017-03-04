4e2d801753ac05adc9d046d7ec824e491cbf5215
tanuki2015 <75954762@qq.com>
2017-03-04 18:02:40 +0800

    exercise todolist 拆分todos reducer

--- exercise/redux/redux教程30/exerciseTodoList.js
+++ exercise/redux/redux教程30/exerciseTodoList.js
@@ -5,30 +5,43 @@ import {createStore} from 'redux';
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
-const todos = (state = [], action) => {
-    switch  (action.type) {
+const todo = (state = {}, action) => {
+    switch (action.type) {
         case 'ADD_TODO':
-            return [
-                ...state,
-                {id: action.id, text: action.text, completed: false}
-            ];
+            return {
+                id: action.id,
+                text: action.text,
+                completed: false
+            };
         case 'TOGGLE_TODO':
-            return state.map(item => {
-                if (item.id !== action.id) {
-                    return item;
-                }
+            if (state.id !== action.id) {
+                return state;
+            }
 
-                return {
-                    ...item,
-                    completed: !item.completed
-                }
-            });
+            return {
+                ...state,
+                completed: !state.completed
+            };
         default:
             return state;
     }
 };
 
-const testTodos = () => {
+const todos = (state = [], action) => {
+    switch  (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                todo(undefined, action)
+            ];
+        case 'TOGGLE_TODO':
+            return state.map(item => todo(item, action));
+        default:
+            return state;
+    }
+};
+
+const testAddTodo = () => {
     const listBefore = [];
     const action = {type: 'ADD_TODO', id: 0, text: 'wohahaha'};
     const listAfter = [{id: 0, text: 'wohahaha', completed: false}];
@@ -39,5 +52,17 @@ const testTodos = () => {
     expect(todos(undefined, action)).toEqual(listAfter);
 };
 
-testTodos();
+const testToggleTodo = () => {
+    const listBefore = [{id: 0, text: 'wohahaha', completed: false}];
+    const action = {type: 'TOGGLE_TODO', id: 0};
+    const listAfter = [{id: 0, text: 'wohahaha', completed: true}];
+
+    deepFreeze(listBefore);
+    deepFreeze(action);
+
+    expect(todos(listBefore,action)).toEqual(listAfter);
+};
+
+testAddTodo();
+testToggleTodo();
 console.log('all test pasted!');
