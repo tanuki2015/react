2825859f9af6dc0af5b31c9569d12b58eaa749cd
tanuki2015 <75954762@qq.com>
2017-03-04 21:30:20 +0800

    combine todos and setVisibilityFilter

--- exercise/redux/redux教程30/exerciseTodoList.js
+++ exercise/redux/redux教程30/exerciseTodoList.js
@@ -1,10 +1,11 @@
 // 做练习
 import React from 'react';
 import ReactDom from 'react-dom';
-import {createStore} from 'redux';
+import { createStore, combineReducers } from 'redux';
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
+// reducer，被todos reducer调用，处理todos中的todo项目，typeof {}
 const todo = (state = {}, action) => {
     switch (action.type) {
         case 'ADD_TODO':
@@ -27,6 +28,7 @@ const todo = (state = {}, action) => {
     }
 };
 
+// reducer，处理state中todos，typeof array
 const todos = (state = [], action) => {
     switch  (action.type) {
         case 'ADD_TODO':
@@ -41,6 +43,35 @@ const todos = (state = [], action) => {
     }
 };
 
+// reducer，处理state中visibilityFilter，typeof string
+const setVisibilityFilter = (state = 'SHOW_ALL', action) => {
+    switch  (action.type) {
+        case 'SET_VISIBILITY_FILTER':
+            return action.filter;
+        default:
+            return state;
+    }
+};
+
+// combineReducers
+const rootReducer = combineReducers({
+    todos,
+    setVisibilityFilter,
+});
+
+// create todoApp component
+
+
+// create store
+const store = createStore(rootReducer);
+
+const render = () => {
+    ReactDom.render(
+
+    )
+}
+
+// 下面是test...
 const testAddTodo = () => {
     const listBefore = [];
     const action = {type: 'ADD_TODO', id: 0, text: 'wohahaha'};
@@ -63,6 +94,13 @@ const testToggleTodo = () => {
     expect(todos(listBefore,action)).toEqual(listAfter);
 };
 
+const testSetVisibilityFilter = () => {
+    const visibilityBefore = 'SHOW_ALL';
+    const visibilityAfter = 'SHOW_ACTIVE';
+    const action = {type: 'SET_VISIBILITY_FILTER', filter: 'SHOW_ACTIVE'};
+    expect(setVisibilityFilter(visibilityBefore, action)).toEqual(visibilityAfter);
+};
 testAddTodo();
 testToggleTodo();
+testSetVisibilityFilter();
 console.log('all test pasted!');
